058b5ab37faec61e30bb5dc3e43ab06bab0c4d38
karthik14344 <singidikarthik1434@gmail.com>
2026-02-21 14:31:41 +0530

    project setup ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Karthik Singidi",
+    author_email="singidikarthik1434@gmail.com",
+    packages=find_packages()
+)
